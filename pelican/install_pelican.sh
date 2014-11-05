7ee49a12d9015bd966c50eb677a3c3c93348b62c
Owen Campbell <owen.campbell@empiria.co.uk>
2014-11-05 11:01:58 +0000

    corrects paths to new folders

--- pelican/install_pelican.sh
+++ pelican/install_pelican.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 apt-get install -y python-pip
-pip install -r /vagrant/provision/requirements.txt
-cp /vagrant/provision/*.conf /etc/init
+pip install -r /vagrant/provision/pelican/requirements.txt
+cp /vagrant/provision/pelican/*.conf /etc/init
 start pelican-server
