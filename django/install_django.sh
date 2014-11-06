838fa915250eaa4a2bee7ab4993e2a7fe1e17535
Owen Campbell <owen.campbell@empiria.co.uk>
2014-11-06 16:18:35 +0000

    adds django server

--- django/install_django.sh
+++ django/install_django.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
+# Assign the script arguments to meaningful variables
 DJANGO_PROJECT=$1
 DB_NAME=$2
 DB_USER=$3
 DB_PWD=$4
 
-apt-get install -y python-pip
-apt-get install -y postgresql postgresql-contrib
+# install pip and postgres
+apt-get install -y python-pip postgresql postgresql-contrib
 
+# create the postgres user and database
 cat << EOF | su - postgres -c psql
 CREATE USER $DB_USER WITH PASSWORD '$DB_PWD';
 
 CREATE DATABASE $DB_NAME WITH OWNER $DB_USER;
 EOF
 
+# install django and its dependencies
 pip install -r /vagrant/provision/django/requirements.txt
 
+# create the django project from my template
 cd /vagrant
 django-admin startproject --template=https://github.com/meatballs/django-vagrant-template/archive/master.zip $DJANGO_PROJECT
+
+# configure the django dev server as an upstart daemon
+cp /vagrant/provision/django/django-server.conf /etc/init
+sed -i "s/{{ project_name }}/$DJANGO_PROJECT/" /etc/init/django-server.conf
+start django-server
