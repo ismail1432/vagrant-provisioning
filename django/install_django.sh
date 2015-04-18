1d5fbfb46c415806cc2e22729268e1b44ea7ca67
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-18 16:28:13 +0100

    corrects variables

--- django/install_django.sh
+++ django/install_django.sh
@@ -2,17 +2,14 @@
 
 # Assign the script arguments to meaningful variables
 DJANGO_PROJECT=$1
-DB_NAME=$2
-DB_USER=$3
-DB_PWD=$4
-REPO_URL=$5
+REPO_URL=$2
 
 # install pip
 apt-get install -y python-pip
 
 # create the django project
 cd /vagrant
-git clone $5
+git clone $2
 
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
