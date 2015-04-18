a1d3955e6af5f3f82d9c7f0b9f39c630c8e0face
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-18 17:26:54 +0100

    adds db creation

--- django/install_django.sh
+++ django/install_django.sh
@@ -7,8 +7,14 @@ DJANGO_PROJECT=$1
 # install pip
 apt-get install -y python-pip
 
-# install python packages from requirements
+# install python packages
 pip install -r requirements.txt
+
+# Create database
+su postgres -c "createuser -w -d -r -s $DJANGO_PROJECT"
+su postgres -c "createdb -O $DJANGO_PROJECT $DJANGO_PROJECT"
+
+# Set environment variables
 
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
