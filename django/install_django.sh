0bc12bbb9f6509aea26ae4e620bff1675c2e785e
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-18 16:27:30 +0100

    adds cloning of git repo

--- django/install_django.sh
+++ django/install_django.sh
@@ -5,9 +5,14 @@ DJANGO_PROJECT=$1
 DB_NAME=$2
 DB_USER=$3
 DB_PWD=$4
+REPO_URL=$5
 
 # install pip
 apt-get install -y python-pip
+
+# create the django project
+cd /vagrant
+git clone $5
 
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
