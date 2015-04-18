b50234644ec2705f6cee7c8dd5770e31af0c6c4f
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-18 16:38:22 +0100

    gets rid of redundant cloning stuff

--- django/install_django.sh
+++ django/install_django.sh
@@ -2,14 +2,9 @@
 
 # Assign the script arguments to meaningful variables
 DJANGO_PROJECT=$1
-REPO_URL=$2
 
 # install pip
 apt-get install -y python-pip
-
-# create the django project
-cd /vagrant
-git clone $2
 
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
