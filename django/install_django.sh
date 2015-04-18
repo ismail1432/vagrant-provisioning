111b9226b2b5186385a861afd0bf856929b8a061
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-18 16:48:31 +0100

    adds pip install from requirements.txt

--- django/install_django.sh
+++ django/install_django.sh
@@ -6,6 +6,9 @@ DJANGO_PROJECT=$1
 # install pip
 apt-get install -y python-pip
 
+# install python packages from requirements
+pip install -r requirements.txt
+
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
 sed -i "s/{{ project_name }}/$DJANGO_PROJECT/" /etc/init/django-server.conf
