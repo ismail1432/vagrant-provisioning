84deadf48106bb9b3e5ed4e21594dce8e50c8233
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-19 19:59:49 +0100

    moves them back again

--- django/install_django.sh
+++ django/install_django.sh
@@ -10,13 +10,18 @@ apt-get install -y python-pip
 # install python packages
 pip install -r /vagrant/requirements.txt
 
+# Set environment variables
+export DEBUG=TRUE
+export SECRET_KEY="LousyKeyOnlySuitableForDevEnvironments"
+export DATABASE_URL="postgres://djaxelrod:djaxelrod@localhost/djaxelrod"
+
 # Create database
 su postgres -c "createuser -w -d -r -s $DJANGO_PROJECT"
 sudo -u postgres psql -c "ALTER USER $DJANGO_PROJECT WITH PASSWORD '$DJANGO_PROJECT';"
 su postgres -c "createdb -O $DJANGO_PROJECT $DJANGO_PROJECT"
 cd /vagrant
-python manage.py migrate
 
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
 start django-server
+
