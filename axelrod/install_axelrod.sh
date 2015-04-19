f9590a8e298c8cb4340e330b3465caeb82fb7723
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-19 12:23:55 +0100

    removes axelrod installation in favour of pkg-config

--- axelrod/install_axelrod.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Starting Axelrod dependencies installation"
-
-apt-get install -y libfreetype6-dev libpng12-dev
--- django/install_django.sh
+++ django/install_django.sh
@@ -15,6 +15,9 @@ su postgres -c "createuser -w -d -r -s $DJANGO_PROJECT"
 su postgres -c "createdb -O $DJANGO_PROJECT $DJANGO_PROJECT"
 
 # Set environment variables
+echo "export DEBUG='True'" >> ~/.zshrc
+echo "export DATABASE_URL='postgres://djaxelrod@localhost/djaxelrod'" >> ~/.zshrc
+echo "export SECRET_KEY='NeedToComeUpWithABetterSecretKey'" >> ~/.zshrc
 
 # configure the django dev server as an upstart daemon
 cp /vagrant/provision/django/django-server.conf /etc/init
