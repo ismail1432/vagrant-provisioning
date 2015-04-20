948f1192e013576c3c18d0ed0edd058285c58329
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-20 15:10:55 +0100

    Adds config for remote connections to postgres

--- postgresql/install_postgresql.sh
+++ postgresql/install_postgresql.sh
@@ -2,3 +2,7 @@
 echo "Starting Postgresql installation"
 
 apt-get -y install postgresql postgresql-contrib libpq-dev python-dev
+
+sudo sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.3/main/postgresql.conf
+echo "host    all             all             0.0.0.0/0               md5" | sudo tee -a /etc/postgresql/9.3/main/pg_hba.conf
+sudo service postgresql restart
