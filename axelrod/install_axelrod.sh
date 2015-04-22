76299324c7180b9c9358eb33d6e6357c791e5eac
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-22 20:06:36 +0100

    removes remaining axelrod provision

--- axelrod/install_axelrod.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "***************************************"
-echo "*** Installing Axelrod dependencies ***"
-echo "***************************************"
-
-apt-get install -y libfreetype6-dev libpng12-dev
