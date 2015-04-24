0e646830c7cf585629ace8da5a72bbd38e564c28
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-24 13:42:44 +0100

    adds nodejs upstart daemon

--- nodejs/install_nodejs.sh
+++ nodejs/install_nodejs.sh
@@ -11,3 +11,7 @@ sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 # install the grunt cli
 npm install -g grunt-cli
+
+# configure the nodejs dev server as an upstart daemon
+cp /vagrant/provision/nodejs/nodejs-server.conf /etc/init
+start nodejs-server
