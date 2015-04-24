59e08a6b31bb6bdc890e87b62fa773b995cf1871
Owen Campbell <owen.campbell@empiria.co.uk>
2015-04-24 13:10:11 +0100

    adds nodejs provision

--- nodejs/install_nodejs.sh
+++ nodejs/install_nodejs.sh
@@ -3,4 +3,11 @@ echo "*****************************"
 echo "*** Installing NodeJs ***"
 echo "*****************************"
 
+# install node and npm
 apt-get -y install nodejs npm
+
+# sort out the nodejs vs node naming issue
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+# install the grunt cli
+npm install -g grunt-cli
